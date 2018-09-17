ea54a6c48c88594e725bafb7a470cde7c4533055
wuqian <1307144731@qq.com>
2018-09-17 11:17:49 +0800

    去除不必要的文件

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,5 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
     <router-view/>
   </div>
 </template>
-
-<script>
-export default {
-  name: 'App',
-};
-</script>
-
-<style>
-/* #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-} */
-</style>
